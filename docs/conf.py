a01197a9279a7bd2164d6120f8e6f8b9a99de328
James Munroe <jmunroe@mun.ca>
2017-05-31 00:40:43 +0000

    converting to sphinx-gallery

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,8 @@ sys.path.insert(0, os.path.abspath('.'))
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['cookbookrebuild']
+extensions = [#'cookbookrebuild',
+              'sphinx_gallery.gen_gallery',]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -47,8 +48,8 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'cosima-cookbook'
-copyright = '2017, cosima'
-author = 'cosima'
+copyright = '2017, COSIMA'
+author = 'COSIMA'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -155,3 +156,7 @@ texinfo_documents = [
 
 
 
+sphix_gallery_conf = {
+    'example_dirs' : '../examples',
+    'gallery_dirs' : 'auto_examples',
+    }
