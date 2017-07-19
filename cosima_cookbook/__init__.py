1fc093034997a3fd95147e7b8c348aafc7b53b00
James Munrore <jmunroe@mun.ca>
2017-07-19 11:02:22 +1000

    rewrite of get_nc_variable to use central database

--- cosima_cookbook/__init__.py
+++ cosima_cookbook/__init__.py
@@ -21,18 +21,20 @@ from joblib import Memory
 
 from tqdm import tqdm_notebook, tqdm
 
+client = Client()
+
 cachedir = tempfile.gettempdir()
 memory = Memory(cachedir=cachedir, verbose=0)
 
-import warnings 
-warnings.filterwarnings("ignore", 
+import warnings
+warnings.filterwarnings("ignore",
                         message="Unable to decode time axis into full numpy.datetime64 objects")
 
 # hardcoded -- this is meant to be run on a NCI system
 DataDir = '/g/data3/hh5/tmp/cosima'
 
 # find all experiments with at least one outputNNN subdirectory
-expts = sorted({ re.search(DataDir + '/' + '(.*)' + '/output', d).group(1) 
+expts = sorted({ re.search(DataDir + '/' + '(.*)' + '/output', d).group(1)
 	         for d in glob(os.path.join(DataDir , '*/*/output*') )})
 
 def get_expt():
@@ -49,106 +51,31 @@ def index_ncfile(ncpath):
     """
 
     ncfile = os.path.basename(ncpath)
- 
+
     # extract out experiment from path
-    expt = re.search(DataDir + '/' + '(.*)' + '/output', ncpath).group(1) 
+    expt = re.search(DataDir + '/' + '(.*)' + '/output', ncpath).group(1)
 
     if not os.path.exists(ncpath):
         return None
-    
+
     dataset = netCDF4.Dataset(ncpath)
- 
+
     index = [ { 'ncfile': ncfile,
                 'ncpath': ncpath,
                 'configuration' : '',
                 'experiment' : expt,
-                'run' : '', 
+                'run' : '',
                 'name' : v.name,
                 'dimensions' : v.dimensions,
                 'chunking' : tuple(v.chunking()), }
         for v in dataset.variables.values() ]
 
-    dataset.close()   
+    dataset.close()
 
     return index
 
-@memory.cache    
-def build_index():
-    """
-    An experiment is a collection of outputNNN directories.  Each directory 
-    represents the output of a single job submission script. These directories 
-    are created by the *payu* tool.   
-
-    An experiment is given by a modelconfiguration/experimentname
- 
-    The file `diag_table` identifies which fields should be in the output directory.
- 
-    But we can also examine the .nc files directly to infer their contents.
-    for each .nc file, get variables -> dimensions
- 
-    .ncfile, varname, dimensions, chunksize
-
-    Generate an index for all netCDF4 files. The results are cached, so 
-    this needs only to be done once.
-    """
-
-    ncfiles = glob(os.path.join(DataDir, '*/*/output*/*.nc'))
-    ncfiles.sort()
-    bag = dask.bag.from_sequence(ncfiles)
-    index = bag.map(index_ncfile)
-   
-    # convert bag to list
-    index = list(index)
-
-    # flatten
-    index = [item for sublist in index for item in sublist]
-
-    # convert to dataframe
-    index = pd.DataFrame.from_records(index)
-
-    return index
-
-
-def get_nc_variable(expt, ncfile, variable, chunks={}, n=None,
-                   op= lambda x: x):
-    """
-    For a given experiment, concatenate together variable over all time
-    given a basename ncfile.
-    
-    By default, xarray is set to use the same chunking pattern that is
-    stored in the ncfile. This can be overwritten by passing in a dictionary
-    chunks or setting chunks=None for no chunking (load directly into memory).
-    
-    n > 0 means only use the last n ncfiles files. Useful for testing.
-    
-    op() is function to apply to each variable before concatenating.
-    
-    """
-    
-    df = build_index()
-
-    var = df[(df.ncfile.str.contains(ncfile)) & (df.name == variable)]
-    
-    chunking = var.chunking.iloc[0]
-    dimensions = var.dimensions.iloc[0]  
-    default_chunks = dict(zip(dimensions, chunking))
-    
-    if chunks is not None:
-        default_chunks.update(chunks)
-        chunks = default_chunks
-        
-    ncfiles = sorted(list(var.ncpath))
-    
-    if n is not None:
-        ncfiles = ncfiles[-n:]
-
-    b = dask.bag.from_sequence(ncfiles)
-    b = b.map(lambda fn : op(xr.open_dataset(fn, chunks=chunks)[variable]) )
-    datasets = b.compute()
-    
-    dsx = xr.concat(datasets, dim='time', coords='all')
-
-    return dsx
+from .netcdf_index import build_index
+from .netcdf_index import get_nc_variable
 
 class Configuration():
     pass
