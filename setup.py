7607f254b860d1824a559436b758483cd693d50e
James Munroe <jmunroe@mun.ca>
2017-07-28 16:36:17 +1000

    modify setup.py to find_packages

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setup(name='cosima_cookbook',
-      version='0.1',
-      description='Diagnostics for COSIMA: Consortium for Ocean-Sea Ice Modelling in Australia',
-      url='https://github.com/OceansAus/cosima-cookbook',
-      author='James Munroe',
-      author_email='jmunroe@mun.ca',
-      license='Apache License 2.0',
-      packages=['cosima_cookbook'],
-      zip_safe=False)
+setup(
+    name='cosima_cookbook',
+    version='0.1',
+    description='Diagnostics for COSIMA: Consortium for Ocean-Sea Ice Modelling in Australia',
+    url='https://github.com/OceansAus/cosima-cookbook',
+    author='James Munroe',
+    author_email='jmunroe@mun.ca',
+    license='Apache License 2.0',
+    packages=find_packages(),
+)
