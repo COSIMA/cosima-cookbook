95f9739b65075b775fb31c4623f5fb1b84e68560
Andrew <31054815+aekiss@users.noreply.github.com>
2017-09-08 14:27:56 +1000

    fixes to handle missing .nml files

--- cosima_cookbook/summary/nml_diff.py
+++ cosima_cookbook/summary/nml_diff.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
 
+# Tools to read a set of namelist files and find their superset and difference.
+# The functions are general-purpose (i.e. no ACCESS-OM2-related assumptions).
+# Andrew Kiss https://github.com/aekiss
+
 
 import f90nml  # from http://f90nml.readthedocs.io/en/latest/
 import os
@@ -10,7 +14,7 @@ def nmldict(nmlfnames):
         FORTRAN namelist files.
 
     Input: tuple of any number of namelist file path strings
-            non-existent files are silently ignored
+            (non-existent files are silently ignored)
     Output: dict with key:value pairs where
             key is filename path string
             value is complete Namelist from filename
@@ -49,16 +53,15 @@ def superset(nmlall):
 
 
 def nmldiff(nmlall):
-    """Return dict of the groups/group members that differ across multiple
-        FORTRAN namelist files.
+    """Remove every group/group member that is the same in all file Namelists.
 
     Input: dict (e.g. returned by nmldict) with key:value pairs where
             key is filename path string
             value is complete Namelist from filename
-    Output: dict with key:value pairs where
+    Output: modified input dict with key:value pairs where
             key is filename strings
-            value is Namelist from filename, with anything common to
-                all other files removed
+            value is Namelist from filename, with any group/group member
+                common to all other files removed
     """
 
 # Create diff by removing common groups/members from nmlall.
@@ -76,29 +79,29 @@ def nmldiff(nmlall):
     for group in nmlsuperset:
         # init: whether group is present and identical in all namelist files
         deletegroup = True
-        for nml in nmlfnames:
+        for nml in nmlall:
             deletegroup = deletegroup and (group in nmlall[nml])
         if deletegroup:  # group present in all namelist files
             for mem in nmlsuperset[group]:
                 # init: whether group member is present and identical
                 #   in all namelist files
                 deletemem = True
-                for nml in nmlfnames:
+                for nml in nmlall:
                     deletemem = deletemem and (mem in nmlall[nml][group])
                 if deletemem:  # group member is present in all namelist files
-                    for nml in nmlfnames:
+                    for nml in nmlall:
                         # ... now check if values match in all namelist files
                         deletemem = deletemem and \
                             (nmlall[nml][group][mem] ==
                              nmlsuperset[group][mem])
                     if deletemem:
-                        for nml in nmlfnames:
+                        for nml in nmlall:
                             # delete mem from this group in all nmls
                             del nmlall[nml][group][mem]
-            for nml in nmlfnames:
+            for nml in nmlall:
                 deletegroup = deletegroup and (len(nmlall[nml][group]) == 0)
             if deletegroup:
                 # group is common to all nmls and now empty so delete
-                for nml in nmlfnames:
+                for nml in nmlall:
                     del nmlall[nml][group]
     return nmlall
