31aa0e49975ed9fab0984bf83798e5970cb3c7ae
andy <Andy.Hogg@anu.edu.au>
2017-09-14 20:05:19 +1000

    diagnostics function for drake passage transport

--- cosima_cookbook/diagnostics/__init__.py
+++ cosima_cookbook/diagnostics/__init__.py
@@ -1,5 +1,5 @@
 from . mean_tau_x import mean_tau_x
-from . simple import annual_scalar
+from . simple import annual_scalar, drake_passage
 from . overturning import psi_avg
 
-__all__ = ['mean_tau_x', 'annual_scalar']
+__all__ = ['mean_tau_x', 'annual_scalar', 'drake_passage']
