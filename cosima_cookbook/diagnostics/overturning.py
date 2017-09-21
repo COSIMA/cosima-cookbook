bbb7af47e6ad4748bb6b4d75a48f3098ba27bba8
andy <Andy.Hogg@anu.edu.au>
2017-09-21 17:15:21 +1000

    modified overturning functions

--- cosima_cookbook/diagnostics/overturning.py
+++ cosima_cookbook/diagnostics/overturning.py
@@ -1,20 +1,20 @@
 from joblib import Memory
 
 cachedir = None
-memory = Memory(cachedir=cachedir, verbose=0)
+memory = Memory(cachedir=cachedir, verbose=1)
 
 from ..netcdf_index import get_nc_variable
 
 @memory.cache
 def psi_avg(expt, n=10):
 
-    # modified to remove op by AH - 20/9/17
-    #op = lambda p: p.sum('grid_xt_ocean').cumsum('potrho')
-    #psi = get_nc_variable(expt, 'ocean.nc', 'ty_trans_rho',
-    #                      op=op,
-    #                      chunks={'potrho': None}, n=25)
-    
+    def op(p):
+        summed_p = p.sum('grid_xt_ocean')
+        summed_p.attrs['units'] = p.units
+        return summed_p
+
     psi = get_nc_variable(expt, 'ocean.nc', 'ty_trans_rho',
+                          op=op,
                           chunks={'potrho': None}, n=n,
                           time_units = 'days since 1900-01-01')
     
@@ -22,8 +22,8 @@ def psi_avg(expt, n=10):
         #print('WARNING: Changing units for ', expt)
         psi = psi*1.0e-9
     
-    psi_avg = psi.sum('grid_xt_ocean').cumsum('potrho').mean('time') - \
-                psi.sum('grid_xt_ocean').sum('potrho').mean('time')
+    psi_avg = psi.cumsum('potrho').mean('time') - \
+                psi.sum('potrho').mean('time')
     psi_avg = psi_avg.compute()
 
     return psi_avg
@@ -32,24 +32,26 @@ def psi_avg(expt, n=10):
 @memory.cache
 def psiGM_avg(expt, n=10):
 
-    # modified to remove op by AH - 20/9/17
-    #op = lambda p: p.sum('grid_xt_ocean').cumsum('potrho')
-    #psi = get_nc_variable(expt, 'ocean.nc', 'ty_trans_rho',
-    #                      op=op,
-    #                      chunks={'potrho': None}, n=25)
+    def op(p):
+        summed_p = p.sum('grid_xt_ocean')
+        summed_p.attrs['units'] = p.units
+        return summed_p
     
     psiGM = get_nc_variable(expt, 'ocean.nc', 'ty_trans_rho_gm',
-                            chunks={'potrho': None}, n=n,
-                            time_units = 'days since 1900-01-01')
+                          op=op,
+                          chunks={'potrho': None}, n=n,
+                          time_units = 'days since 1900-01-01')
     
     if psiGM.units == 'kg/s':
-        #print('WARNING: Changing units for ', expt)
+        #print('WARNING: Changing psiGM units for ', expt)
         psiGM = psiGM*1.0e-9
     
-    psiGM_avg = psiGM.sum('grid_xt_ocean').mean('time')
+    psiGM_avg = psiGM.mean('time')
     psiGM_avg = psiGM_avg.compute()
 
     return psiGM_avg
+
+
 
 @memory.cache
 def zonal_mean(expt, variable, n=10):
