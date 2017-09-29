681a299cbbe06fd4027eec2dd6914831d9c77ea5
James Munrore <jmunroe@mun.ca>
2017-09-29 17:05:19 +1000

    update diagnostic notebooks to work with package functions

--- cosima_cookbook/distributed.py
+++ cosima_cookbook/distributed.py
@@ -14,7 +14,7 @@ def start_cluster(diagnostics_port=0):
     n_workers = os.cpu_count() // 2
     cluster = LocalCluster(ip='localhost',
                        n_workers=n_workers,
-                       diagnostics_port=bokeh_port,
+                       diagnostics_port=diagnostics_port,
                        memory_limit=6e9)
     client = Client(cluster)
 
@@ -23,7 +23,7 @@ def start_cluster(diagnostics_port=0):
            'scheduler_ip': cluster.scheduler.ip,
            'hostname': hostname, }
 
-    print("Run this command on a local terminal to set up SSH tunnels for dashboard:")
+    print("If the link to the dashboard below doesn't work, run this command on a local terminal to set up a SSH tunnel:")
     print()
     print("  ssh -N -L {bokeh_port}:{scheduler_ip}:{bokeh_port} {hostname}.nci.org.au -l {user}".format(**params) )
     
