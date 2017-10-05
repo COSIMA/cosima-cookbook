b050b5186cfe3f75d4cda1b34e6722d8df3b22e8
andy <Andy.Hogg@anu.edu.au>
2017-10-05 23:01:48 +1100

    Added RYF analysis notebook

--- cosima_cookbook/diagnostics/simple.py
+++ cosima_cookbook/diagnostics/simple.py
@@ -8,7 +8,7 @@ from ..netcdf_index import get_nc_variable
 @memory.cache
 def annual_scalar(expt, variable):
     darray = get_nc_variable(expt, 'ocean_scalar.nc', variable,
-                              time_units='days since 2000-01-01')
+                              time_units='days since 1900-01-01')
     annual_average = darray.resample('A', 'time').load()
     annual_average.attrs['long_name'] = darray.long_name + ' (annual average)'
     annual_average.attrs['units'] = darray.units
