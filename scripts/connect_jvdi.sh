32c1e52110da848a35d7c0f8736fc131d7b0d1eb
Josue-Martinez-Moreno <josue.martinezmoreno@anu.edu.au>
2018-05-09 15:56:25 +1000

    VDI user config file and BASH connection to VDI

--- scripts/connect_jvdi.sh
+++ scripts/connect_jvdi.sh
@@ -8,32 +8,40 @@
 # where $COSIMA_COOKBOOK_PATH is the path to the cosima cookbook directory.
 # To run just type in the command line "connect_jvdi" after loading the functions. 
 
-uconfigpath=~/vdiuser.config
+uconfigpath=~/cosima_cookbook.conf
 
 function get_session(){
     if [ -f ${uconfigpath} ]; then
-	user="$(grep user ${uconfigpath} | cut -d ':' -f2 | tr -d ' ",')" 
+	user="$(grep user ${uconfigpath} | cut -d '=' -f2 | tr -d ' ')" 
     else
-	#Change the user in the following lines and comment the next line.
-	echo Check the user or set up a vdiuser.config at your home directory.
-        user="$(echo "ur4354")"
+        echo "[DEFAULT]" > ${uconfigpath}
+	echo "user = " $USER >> ${uconfigpath}
+	echo jupyterport = 8889 >> ${uconfigpath}
+        echo bokehport = 8787 >> ${uconfigpath}
+        echo exechost = vdi.nci.org.au >> ${uconfigpath}
     fi
-    echo -n "Determine if VDI session is already running..."
-    running="$(ssh -o LogLevel=QUIET -t $user@vdi.nci.org.au "/opt/vdi/bin/session-ctl --configver=20151620513 list-avail --partition main")"
-    if [ ! -z "$running" ];then
-	echo "True"
-	idnum="$(echo $running | sed -e 's/#~#id=\([0-9]*\).*/\1/')"
+    pingin="$(ssh -o LogLevel=QUIET -t $user@vdi.nci.org.au "/opt/vdi/bin/session-ctl --configver=20151620513 hello --partition main")"
+    if [ ! -z "$pingin" ];then
+        echo -n "Determine if VDI session is already running..."
+        running="$(ssh -o LogLevel=QUIET -t $user@vdi.nci.org.au "/opt/vdi/bin/session-ctl --configver=20151620513 list-avail --partition main")"
+        if [ ! -z "$running" ];then
+	    echo "True"
+	    idnum="$(echo $running | sed -e 's/#~#id=\([0-9]*\).*/\1/')"
+        else
+            echo "False"
+ 	    echo -n  "Launching new VDI session..."
+            running="$(ssh -o LogLevel=QUIET -t $user@vdi.nci.org.au "/opt/vdi/bin/session-ctl --configver=20151620513 launch --partition main")"
+    	    idnum="$(echo $running | sed -e 's/#~#id=\([0-9]*\).*/\1/')"
+        fi
+        echo Determine jobid for VDI session... $idnum
+        echo -n Host for VDI session...
+        session="$(ssh -o LogLevel=QUIET -t $user@vdi.nci.org.au "/opt/vdi/bin/session-ctl --configver=20151620513 get-host --jobid ${idnum}")"
+        sesionnnum="$(echo $session | sed -e 's/#~#host=vdi-n\([0-9]*\).*/\1/')"
+        echo $sesionnnum
     else
-        echo "False"
-	echo -n  "Launching new VDI session..."
-        running="$(ssh -o LogLevel=QUIET -t $user@vdi.nci.org.au "/opt/vdi/bin/session-ctl --configver=20151620513 launch --partition main")"
-	idnum="$(echo $running | sed -e 's/#~#id=\([0-9]*\).*/\1/')"
+	 echo "Incorrect user name in ~/cosima_cookbook.conf file?"
+         echo "Edit ~/cosima_cookbook.conf before continuing."
     fi
-    echo Determine jobid for VDI session... $idnum
-    echo -n Host for VDI session...
-    session="$(ssh -o LogLevel=QUIET -t $user@vdi.nci.org.au "/opt/vdi/bin/session-ctl --configver=20151620513 get-host --jobid ${idnum}")"
-    sesionnnum="$(echo $session | sed -e 's/#~#host=vdi-n\([0-9]*\).*/\1/')"
-    echo $sesionnnum
 }
 
 function connect_jvdi(){
--- scripts/jupyter_vdi.py
+++ scripts/jupyter_vdi.py
@@ -2,18 +2,21 @@
 """
 Script to launch a VDI session (or connect to already running session)
 and start a Jupyter server on the VDI
+
 A ssh tunnel from the local machine to the VDI is set up and the local
 webbrowser is spawned.
+
 This is a python3 script (uses unicode strings).  If you don't have
 python3 on your local machine, try installing Miniconda3
 The only external module is pexpect which may need to be installed
 using conda or pip.
+
 Usage:
-- edit params dictionary below with your NCI user id (replace jm0634)
-- if you use a password, the script will prompt you for your password when needed
+- if you use a password, the script will ask for your password when needed
 - if you have already set up SSH public key with Strudel, try running
     $ ssh-add ~/.ssh/MassiveLauncherKey
   to add your public key to the ssh key agent.
+
 Author: James Munroe, 2017
 """
 
@@ -26,55 +29,87 @@ import time
 import getpass
 
 import pexpect
-#Check Version of MAC OS
-from appscript import *
-import platform
-import json
-OS_c=platform.system()
-OS_V=platform.release()
-from os.path import expanduser,join
-home = expanduser("~")
 
-try:
-    params = json.load(open(join(home,'vdiuser.config')))
-except:
-    params = {'user' : 'ur4354',
+import logging
+logging.basicConfig(format='[%(asctime)s jupyter_vdi.py] %(message)s',
+                    datefmt='%H:%M:%S',
+                    level=logging.INFO)
+
+import platform
+OS_c = platform.system()
+OS_v = platform.release()
+
+# Check Version of MAC OS
+if OS_c == 'Darwin':
+    import appscript
+
+import os
+import configparser
+
+DEFAULTS = {'user' : getpass.getuser(),
     'JupyterPort' : '8889',
     'BokehPort' : '8787',
-    'execHost' :  'vdi.nci.org.au',}
+    'execHost' :  'vdi.nci.org.au'}
+
+parser = configparser.ConfigParser(defaults=DEFAULTS)
+
+config_path = os.path.expanduser('~/cosima_cookbook.conf')
+
+if os.path.exists(config_path):
+    logging.info('Using config file: {}'.format(config_path))
+
+    parser.read(config_path)
+else:
+    logging.warn('No config file found. Creating default', config_path, 'file.')
+    logging.warn('*** Please edit this file as needed. ***')
+    with open(config_path, 'w') as f:
+        parser.write(f)
+
+params = parser.defaults()
+
 
 def ssh(cmd, params, login_timeout=10):
     """
     Run a remote command via SSH
     """
 
-    cmd = ("ssh -l {user} {execHost} " + cmd).format(**params)
+    cmd = ("ssh -l {user} {exechost} " + cmd).format(**params)
     s = pexpect.spawn(cmd)
 
     # SSH pexpect logic taken from pxshh:
-    i = s.expect(["(?i)are you sure you want to continue connecting", "(?i)(?:password)|(?:passphrase for key)",
-        "(?i)permission denied", "(?i)connection closed by remote host", pexpect.EOF, pexpect.TIMEOUT], timeout=login_timeout)
+    i = s.expect(["(?i)are you sure you want to continue connecting",
+                  "(?i)(?:password)|(?:passphrase for key)",
+                  "(?i)permission denied",
+                  "(?i)connection closed by remote host",
+                  pexpect.EOF, pexpect.TIMEOUT], timeout=login_timeout)
 
     # First phase
-    if i==0:
-	# New certificate -- always accept it.
-	# This is what you get if SSH does not have the remote host's
-	# public key stored in the 'known_hosts' cache.
+    if i == 0:
+        # New certificate -- always accept it.
+        # This is what you get if SSH does not have the remote host's
+        # public key stored in the 'known_hosts' cache.
         s.sendline("yes")
-        i = s.expect(["(?i)are you sure you want to continue connecting", "(?i)(?:password)|(?:passphrase for key)",
-          "(?i)permission denied", "(?i)connection closed by remote host", pexpect.EOF, pexpect.TIMEOUT], timeout=login_timeout)
+        i = s.expect(["(?i)are you sure you want to continue connecting",
+                      "(?i)(?:password)|(?:passphrase for key)",
+                      "(?i)permission denied",
+                      "(?i)connection closed by remote host",
+                      pexpect.EOF, pexpect.TIMEOUT], timeout=login_timeout)
 
-    if i==1: # password or passphrase
+    if i == 1:  # password or passphrase
         if 'password' not in params:
             params['password'] = getpass.getpass('password: ')
 
         s.sendline(params['password'])
-        i = s.expect(["(?i)are you sure you want to continue connecting", "(?i)(?:password)|(?:passphrase for key)",
-              "(?i)permission denied", "(?i)connection closed by remote host", pexpect.EOF, pexpect.TIMEOUT], timeout=login_timeout)
+        i = s.expect(["(?i)are you sure you want to continue connecting",
+                      "(?i)(?:password)|(?:passphrase for key)",
+                      "(?i)permission denied",
+                      "(?i)connection closed by remote host",
+                      pexpect.EOF, pexpect.TIMEOUT], timeout=login_timeout)
 
     # TODO: check if ssh connection is successful
 
     return s
+
 
 def session(func, *args, **kwargs):
     """wrapper for sending session-ctl commands"""
@@ -83,68 +118,95 @@ def session(func, *args, **kwargs):
     s.close()
     return s
 
-print("Checking SSH keys to VDI are configured...", end='' )
+
+logging.info("Checking SSH keys to VDI are configured...")
 r = session('hello --partition main', params)
 if r.exitstatus != 0:
     # suggest setting up SSH keys
-    # TODO: get user id from user instead of being hard coded
-    print("Error with ssh keys/password and VDI. Edit params dictionary in the script")
+    logging.error("Error with ssh keys/password and VDI.")
+    logging.error("  Incorrect user name in ~/cosima_cookbook.conf file?")
+    logging.error("  Edit ~/cosima_cookbook.conf before continuing.")
     sys.exit(1)
-print("OK")
+logging.info("SSH keys configured OK")
 
-print("Determine if VDI session is already running...", end='')
+logging.info("Determine if VDI session is already running...")
 r = session('list-avail --partition main', params)
 m = re.search('#~#id=(?P<jobid>(?P<jobidNumber>.*?))#~#state=(?P<state>.*?)(?:#~#time_rem=(?P<remainingWalltime>.*?))?#~#', r.before.decode())
 if m is not None:
     params.update(m.groupdict())
     w = int(params['remainingWalltime'])
-    remainingWalltime = '{:02}:{:02}:{:02}'.format(w // 3600, w % 3600 // 60, w % 60)
-    print(remainingWalltime, 'time remaining')
+    remainingWalltime = '{:02}:{:02}:{:02}'.format(
+        w // 3600, w % 3600 // 60, w % 60)
+    logging.info('Time remaining: %s', remainingWalltime)
 
-    # TODO: should give use option of starting a new session of the remaining walltime is short
+    # TODO: should give user option of starting a new session if the remaining walltime is short
 else:
-    print('No')
-    print("Launching new VDI session...", end='')
+    logging.info('No VDI session found')
+    logging.info("Launching a new VDI session...")
     r = session('launch --partition main', params)
-    m = re.search('#~#id=(?P<jobid>(?P<jobidNumber>.*?))#~#', r.before.decode())
+    m = re.search('#~#id=(?P<jobid>(?P<jobidNumber>.*?))#~#',
+                  r.before.decode())
     params.update(m.groupdict())
-    time.sleep(2) # TODO: instead of waiting, should check for confirmation
+    time.sleep(2)  # TODO: instead of waiting, should check for confirmation
     # use has-started
 
-print("Determine jobid for VDI session...{jobid}".format(**params))
+logging.info("Determine jobid for VDI session...{jobid}".format(**params))
 
-print("Get execHost for VDI session...", end='')
+logging.info("Get exechost for VDI session...")
 r = session('get-host --jobid {jobid}', params)
-m = re.search('#~#host=(?P<execHost>.*?)#~#', r.before.decode())
+m = re.search('#~#host=(?P<exechost>.*?)#~#', r.before.decode())
 params.update(m.groupdict())
-print('{execHost}'.format(**params))
+logging.info('exechost: {exechost}'.format(**params))
 
 # wait for jupyter to start running and launch web browser locally
 webbrowser_started = False
+
+
 def start_jupyter(s):
     global webbrowser_started
+
     if not webbrowser_started:
         m = re.search('http://(?P<url>.*)',s.decode('utf8'))
+        print(m)
         if m is not None:
             params.update(m.groupdict())
-            print(OS_c)
-            if OS_c!='Darwin':
+            if not (OS_c == 'Darwin'):
                 # Open browser locally
                 webbrowser.open('http://'+params['url'])
                 webbrowser_started = True
             else:
-                safari=app("Safari")
-                safari.make(new=k.document,with_properties={k.URL:'http://'+params['url']})
+                print('using appscript')
+                safari = appscript.app("Safari")
+                safari.make(new=appscript.k.document, with_properties={
+                            appscript.k.URL: 'http://'+params['url']})
                 webbrowser_started = True
     return s
 
-print ("Running Jupyter on VDI...")
-cmd = """-t -L {JupyterPort}:localhost:{JupyterPort} -L {BokehPort}:localhost:{BokehPort} 'bash -l -c "module load conda/analysis27 && jupyter notebook --no-browser --port {JupyterPort}"'"""
+
+logging.info("Running Jupyter on VDI...")
+
+setupconda = params.get('setupconda',
+              """module use /g/data3/hh5/public/modules
+                 && module load conda/analysis3
+              """.replace('\n', ' '))
+
+jupyterapp = params.get('jupyterapp',  "notebook")
+
+run_jupyter = "jupyter %s --no-browser --port {jupyterport}" % jupyterapp
+
+run_jupyter = setupconda + ' && ' + run_jupyter
+
+cmd = ' '.join(['-t',
+                '-L {jupyterport}:localhost:{jupyterport}',
+                '-L {bokehport}:localhost:{bokehport}',
+                """'bash -l -c "%s"'""" % run_jupyter])
+
 s = ssh(cmd, params, login_timeout=2)
 
-print ("Waiting for Jupyter to start...")
+logging.info("Waiting for Jupyter to start...")
 
 # give control over to user
 s.interact(output_filter=start_jupyter)
 
+logging.info('end of script')
 # optional: terminate to close the vdi session?
