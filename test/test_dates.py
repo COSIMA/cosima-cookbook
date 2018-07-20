86742b496b0b66faed7e31aca294f52ca57bc268
Aidan Heerdegen <aidan.heerdegen@gmail.com>
2018-07-20 16:57:29 +1000

    Added date time tools (#92)
    * Created date_utils for calendar transformations
    
    Also added some tests
    
    * Added offset support, with auto, timedelta and integer as options
    
    Added tests for offsets
    
    * Debugging travis environment
    
    * Fixed test

--- test/test_dates.py
+++ test/test_dates.py
@@ -20,22 +20,119 @@ import pytest
 import sys, os, time, glob
 import shutil
 import pdb # Add pdb.set_trace() to set breakpoints
+import xarray as xr
+import numpy as np
+import cftime
+from datetime import datetime, timedelta
+
+from cosima_cookbook.date_utils import rebase_times, rebase_dataset, \
+    rebase_variable, rebase_shift_attr
+
+from xarray.testing import assert_equal
 
 verbose = True
+
+times = []
 
 def setup_module(module):
     if verbose: print ("setup_module      module:%s" % module.__name__)
     if verbose: print ("Python version: {}".format(sys.version))
     # Put any setup code in here, like making temporary files
+    # Make 5 years of a noleap calendar on the first of each month
+    global times
+    for y in range(1,6):
+        for m in range(1,13):
+            times.append(cftime.date2num(cftime.datetime(y,m,1),units='days since 01-01-01',calendar='noleap'))
+    times = np.array(times)
  
 def teardown_module(module):
     if verbose: print ("teardown_module   module:%s" % module.__name__)
     # Put any taerdown code in here, like deleting temporary files
 
-def test_something():
+def test_rebase_times():
 
-    # Put tests in functions which begin with 'test_'
+    # Should be a 10 year offset between original times and rebased times
+    assert(not np.any((times + 365*10) - rebase_times(times,'days since 1980-01-01','noleap','days since 1970-01-01')))
 
-    # User assert to test conditions
-    assert(True)
+    # Should be a -10 year offset between original times and rebased times
+    assert(not np.any((times - 365*10) - rebase_times(times,'days since 1980-01-01','noleap','days since 1990-01-01')))
+
+def test_rebase_variable():
+
+    timesvar = xr.DataArray(times,attrs={'units':'days since 1980-01-01','calendar':'noleap'})
+
+    print("att:",timesvar.attrs)
+
+    # Test we can rebase with and without explicitly setting a calendar
+    timesvar_rebased = rebase_variable(timesvar, target_units='days since 1970-01-01')
+    assert(timesvar_rebased.equals(rebase_variable(timesvar, 'noleap', target_units='days since 1970-01-01')))
+
+    assert(not timesvar.equals(timesvar_rebased))
+
+    # Should be a 10 year offset between original times and rebased times
+    assert(not np.any((times + 365*10) - timesvar_rebased.values))
+    # assert(not np.any((times + 365*10) - rebase_variable(timesvar, 'noleap', target_units='days since 1970-01-01').values))
+
+    with pytest.raises(ValueError):
+        timesvar_rebased = rebase_variable(timesvar, 'noleap', target_units='days since 1990-01-01')
+
+    # Rebase with an offset otherwise would have negative dates
+    timesvar_rebased = rebase_variable(timesvar, 'noleap', target_units='days since 1990-01-01', offset=365*10)
+
+    # Values should be the same
+    assert(not np.any(times - timesvar_rebased.values))
+
+    # But the rebase_shift_attr should be set to 10 years
+    assert(timesvar_rebased.attrs[rebase_shift_attr] == 365*10)
+
+    # Check we get back timesvar if rebased again with no arguments (rebases to previous
+    # units and applies offset if required in this instance)
+    assert(timesvar.equals(rebase_variable(timesvar_rebased)))
+
+
+def test_matching_time_units():
+
+    testfile = 'test/data/ocean_sealevel.nc'
+
+    ds = xr.open_dataset(testfile,decode_times=False)
+    target_units = 'days since 1800-01-01'
+
+    ds1 = rebase_dataset(ds, target_units)
+    # s1.to_netcdf('tmp.nc')
+
+    ds2 = rebase_dataset(ds1)
+    # ds2.to_netcdf('tmp2.nc')
+
+    # Rebasing again without target_units specified should
+    # un-do previous rebase
+    assert(ds.equals(ds2))
+
+    # An offset is required as the target units are ahead of the data in time
+    target_units = 'days since 2000-01-01'
+
+    # Offset can be automatically generated as difference between target and src units
+    ds1 = rebase_dataset(ds, target_units,offset='auto')
+    ds2 = rebase_dataset(ds1)
+
+    assert(ds.equals(ds2))
+
+    # Offset can be an integer, but need to know what units are being used, days, hours etc
+    ds1 = rebase_dataset(ds, target_units,offset=100*365)
+    ds2 = rebase_dataset(ds1)
+
+    assert(ds.equals(ds2))
+
+    # Offset can be a datetime.timedelta object, but this would need some knowledge of
+    # the calendar
+    ds1 = rebase_dataset(ds, target_units,offset=timedelta(days=100*365))
+    ds2 = rebase_dataset(ds1)
+
+    # A different offset will yield a different dataset, but upon rebasing a second time
+    # should still be the same as the original regardless of offset.
+    ds3 = rebase_dataset(ds, target_units,offset=timedelta(days=200*365))
+    ds4 = rebase_dataset(ds3)
+
+    assert(ds.equals(ds4))
+    assert(not ds1.equals(ds3))
+
 
