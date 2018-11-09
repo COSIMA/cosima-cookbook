46d065261d861f65bfc20dcd1299133143026d43
Angus Gibson <angus@agibson.me>
2018-11-09 14:12:00 +1100

    Allow selection of variables from diagnostic visualiser
    You can now box-select or click individual sections of diagnostic data
    from the visualiser to obtain data about the variable and the range of
    time spanned by the selection. It shouldn't be hard to turn this into
    a copy-pastable string invoking querying.getvar()

--- scripts/diag-vis.py
+++ scripts/diag-vis.py
@@ -1,41 +1,78 @@
 from datetime import datetime
-import sqlite3
+from sys import argv
 
+import cosima_cookbook as cc
 import pandas as pd
+from sqlalchemy import select, distinct, bindparam
 
 from bokeh.io import curdoc
 from bokeh.layouts import column
-from bokeh.models import HoverTool
-from bokeh.models.widgets import Select, Button
+from bokeh.models.callbacks import CustomJS
+from bokeh.models.sources import ColumnDataSource
+from bokeh.models.tools import BoxSelectTool, HoverTool, TapTool
+from bokeh.models.widgets import Select, Button, Div
 import bokeh.palettes
 from bokeh.plotting import figure
 from bokeh.transform import factor_cmap
 
-dbname = '/g/data/v45/ahg157/cosima/cookbook.db'
+if len(argv) < 2:
+    raise Exception('Usage: bokeh serve diag-vis.py --args <db>')
+db = argv[1]
 
-expt_query = 'SELECT DISTINCT experiment FROM ncfiles'
-vars_query = '''SELECT DISTINCT ncvars.variable FROM ncvars
-                LEFT JOIN ncfiles ON ncvars.ncfile = ncfiles.id
-                WHERE ncfiles.experiment = ?'''
-data_query = '''SELECT ncfiles.ncfile, run, variable, time_start, time_end, frequency FROM ncfiles
-                LEFT JOIN ncvars ON ncfiles.id = ncvars.ncfile
-                WHERE ncfiles.experiment = ? AND time_start NOT NULL AND frequency <> 'static'
-                ORDER BY variable, time_start'''
+conn, tables = cc.database.create_database(db)
+
+expt_query = select([distinct(tables['ncfiles'].c.experiment)])
+vars_query = select([distinct(tables['ncvars'].c.variable)]) \
+             .select_from(tables['ncvars'].join(tables['ncfiles'])) \
+             .where(tables['ncfiles'].c.experiment == bindparam('expt'))
+data_query = select([tables['ncfiles'].c.ncfile, tables['ncfiles'].c.run, tables['ncvars'].c.variable,
+                     tables['ncfiles'].c.time_start, tables['ncfiles'].c.time_end, tables['ncfiles'].c.frequency]) \
+             .select_from(tables['ncfiles'].join(tables['ncvars'])) \
+             .where(tables['ncfiles'].c.experiment == bindparam('expt')) \
+             .where(tables['ncfiles'].c.time_start is not None) \
+             .where(tables['ncfiles'].c.frequency != 'static') \
+             .order_by(tables['ncvars'].c.variable, tables['ncfiles'].c.time_start)
+
+expts = [e[0] for e in conn.execute(expt_query)]
 
 def get_data(expt):
-    data = conn.execute(data_query, (expt,)).fetchall()
+    data = conn.execute(data_query, expt=expt).fetchall()
     df = pd.DataFrame(data, columns=['ncfile', 'run', 'variable', 'time_start', 'time_end', 'frequency'])
     df[['time_start', 'time_end']] = df[['time_start', 'time_end']].applymap(
         lambda s: datetime.strptime(s, '%Y-%m-%d %H:%M:%S'))
 
     return df
 
-conn = sqlite3.connect(dbname)
-expts = [e[0] for e in conn.execute(expt_query)]
+def print_selected(div):
+    return CustomJS(args=dict(div=div), code="""
+var source = cb_obj;
+var unique_vars = {};
+for (var i of source.selected['1d'].indices) {
+  var v = source.data['variable'][i];
+  if (v in unique_vars) {
+    unique_vars[v]['time_start'] = Math.min(unique_vars[v]['time_start'], source.data['time_start'][i]);
+    unique_vars[v]['time_end'] = Math.max(unique_vars[v]['time_end'], source.data['time_end'][i]);
+  } else {
+    unique_vars[v] = { time_start: source.data['time_start'][i],
+                       time_end: source.data['time_end'][i] };
+  }
+}
+
+var text = '<table><tr><th>Name</th><th>Start</th><th>End<th></tr>';
+for (var p in unique_vars) {
+  var ts = new Date(unique_vars[p]['time_start']);
+  var te = new Date(unique_vars[p]['time_end']);
+  text = text.concat('<tr><th>'+p+'</th><td>'+ts.toISOString().substr(0,10)+'</td><td>'+te.toISOString().substr(0,10)+'</td></tr>');
+}
+text = text.concat('</table>')
+div.text = text;
+""")
+
 
 # create widgets
-expt_select = Select(title='Experiment:', options=expts, value='1deg_jra55v13_iaf_spinup1_A')
+expt_select = Select(title='Experiment:', options=expts, value=expts[0])
 refresh = Button(label='Update')
+div = Div(width=1000)
 
 # hover tools
 hover = HoverTool(tooltips=[
@@ -45,21 +82,24 @@ hover = HoverTool(tooltips=[
                       'time_start': 'datetime',
                       'time_end': 'datetime'
                       })
-tools = [hover, 'box_select', 'pan', 'box_zoom', 'wheel_zoom', 'reset']
+tap = TapTool()
+box_select = BoxSelectTool()
+tools = [hover, box_select, tap, 'pan', 'box_zoom', 'wheel_zoom', 'reset']
 
 df = get_data(expt_select.value)
 freqs = df.frequency.unique()
 cmap = factor_cmap('frequency', palette=bokeh.palettes.Category10[10], factors=freqs)
+cds = ColumnDataSource(df, callback=print_selected(div))
 
 p = figure(y_range=df.variable.unique(), x_range=(df.iloc[0].time_start, df.iloc[-1].time_end),
            title=expt_select.value, tools=tools)
 cmap = factor_cmap('frequency', palette=bokeh.palettes.Category10[10], factors=freqs)
-hb = p.hbar(y='variable', left='time_start', right='time_end', height=0.4, source=df,
+hb = p.hbar(y='variable', left='time_start', right='time_end', height=0.4, source=cds,
             fill_color=cmap, legend='frequency')
 
 # callback routines to repopulate list of variables
 def get_vars(expt):
-    return [e[0] for e in conn.execute(vars_query, (expt,))]
+    return [e[0] for e in conn.execute(vars_query, expt=expt)]
 
 def refresh_output():
     # get new data
@@ -80,5 +120,5 @@ def refresh_output():
 refresh.on_click(refresh_output)
 
 # layout and show
-layout = column(expt_select, refresh, p)
+layout = column(expt_select, refresh, p, div)
 curdoc().add_root(layout)
