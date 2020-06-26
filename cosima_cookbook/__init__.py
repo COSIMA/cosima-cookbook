7b1f5187333966cbc1249715e1c29d5553e5a450
Angus Gibson <angus@agibson.me>
2020-06-26 15:06:39 +1000

    Add __version__ to module (#171)

--- cosima_cookbook/__init__.py
+++ cosima_cookbook/__init__.py
@@ -3,5 +3,12 @@
 Common tools for working with COSIMA model output
 """
 
+import pkg_resources
+
 from . import database
 from . import querying
+
+try:
+    __version__ = pkg_resources.get_distribution("cosima-cookbook").version
+except Exception:
+    __version__ = "999"
