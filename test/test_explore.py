b387810f1e41d077b874e8cdabb5dbba3feb86a4
Aidan Heerdegen <aidan.heerdegen@anu.edu.au>
2020-08-06 17:04:55 +1000

    Added minimal tests for ExperimentExplorer.
    Modified test files for explore to ensure differences betweem two
    experiments to make tests meaninful.

--- test/test_explore.py
+++ test/test_explore.py
@@ -46,7 +46,7 @@ def session(tmpdir_factory):
                              'test/data/explore/two'], 
                              session)
 
-    # force all files to be marked as present, even though they're empty
+    # force all files to be marked as present, even if they're empty
     ncfiles = session.query(cc.database.NCFile).all()
     for f in ncfiles:
         f.present = True
@@ -62,7 +62,7 @@ def test_database_extension(session):
     de = cc.explore.DatabaseExtension(session=session)
 
     assert(de.experiments.shape == (2,8))
-    assert(de.expt_variable_map.shape == (112,5))
+    assert(de.expt_variable_map.shape == (108,5))
     assert(de.expt_variable_map[de.expt_variable_map.restart].shape == (12,5))
     assert(de.expt_variable_map[de.expt_variable_map.coordinate].shape == (44,5))
     assert(de.keywords == ['access-om2-01', 'another-keyword', 'cosima', 'ryf9091'])
@@ -81,15 +81,15 @@ def test_database_extension(session):
 
     assert(de.experiments.shape == (1,8))
     assert(de.allexperiments.shape == (2,8))
-    assert(de.expt_variable_map.shape == (56,5))
+    assert(de.expt_variable_map.shape == (52,5))
     assert(de.expt_variable_map[de.expt_variable_map.restart].shape == (6,5))
     assert(de.expt_variable_map[de.expt_variable_map.coordinate].shape == (22,5))
     assert(de.keywords == ['access-om2-01', 'another-keyword', 'cosima', 'ryf9091'])
     # All unique variables contained in the database
-    assert(de.variables.shape == (56,5))
+    assert(de.variables.shape == (52,5))
     # Check restart and coordinate variables correctly assigned
-    assert(de.variables[~de.variables.restart & ~de.variables.coordinate].shape == (28,5))
-    assert(de.variables[de.variables.model == 'ocean'].shape == (38,5))
+    assert(de.variables[~de.variables.restart & ~de.variables.coordinate].shape == (24,5))
+    assert(de.variables[de.variables.model == 'ocean'].shape == (34,5))
     assert(de.variables[de.variables.model == 'atmosphere'].shape == (6,5))
     assert(de.variables[de.variables.model == 'ice'].shape == (6,5))
     assert(de.variables[de.variables.model == ''].shape == (6,5)) 
@@ -113,6 +113,7 @@ def test_database_extension(session):
     assert(de.variables[de.variables.model == ''].shape == (12,5)) 
 
 def test_database_explorer(session):
+
     dbx = cc.explore.DatabaseExplorer(session=session)
 
     # Experiment selector
@@ -125,3 +126,25 @@ def test_database_explorer(session):
     variables = dbx.widgets['var_filter'].widgets['selector'].variables
     assert(dbx.widgets['var_filter'].widgets['selector'].widgets['selector'].options == 
            dict(zip(variables[variables.visible].name, variables[variables.visible].long_name)) )
+
+def test_experiment_explorer(session):
+
+    ee = cc.explore.ExperimentExplorer(session=session)
+
+    # Experiment selector
+    assert(ee.widgets['expt_selector'].options == ('one', 'two'))
+
+    assert(len(ee.widgets['var_selector'].widgets['selector'].options) == 23)
+    assert('pot_rho_0' in ee.widgets['var_selector'].widgets['selector'].options)
+    assert('ty_trans_rho' not in ee.widgets['var_selector'].widgets['selector'].options)
+
+    # Simulate selecting a different experiment from menu
+    ee._load_experiment('two')
+    assert(len(ee.widgets['var_selector'].widgets['selector'].options) == 27)
+    assert('pot_rho_0' in ee.widgets['var_selector'].widgets['selector'].options)
+    assert('ty_trans_rho' in ee.widgets['var_selector'].widgets['selector'].options)
+
+
+    print(ee.widgets['frequency'].options)
+    ee.widgets['var_selector'].widgets['selector'].label = 'tx_trans'
+    print(ee.widgets['frequency'].options)
